cadbe819b845c61ede9deabbb55101f93e95095d
Youssef Amr <youssefamr4077@gmail.com>
2024-08-26 00:23:53 +0300

    Update server.js

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,1 +1,10 @@
-console.log("ay haga");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
